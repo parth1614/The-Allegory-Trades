d763b569640485e2941e785d07e47a7772f5c614
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-09 01:39:44 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+function App() {
+  return (
+    <div className="App">
+
+
+      <div className="app_header">
+        <Header />
+      </div>
+
+
+      <div className="app_body">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
