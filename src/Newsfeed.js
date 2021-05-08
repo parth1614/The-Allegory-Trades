fa7f118990058207b46d343d804411b2c4ceaa59
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-09 03:35:28 +0530

    Add files via upload

---
+++ src/Newsfeed.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Newsfeed.css'
+
+function Newsfeed(){
+
+    return (
+        <div className="newsfeed">
+            <div className="newsfeed_container">
+
+                <div className="newsfeed_chartSection">
+                    <div className="newsfeed_portfolio">
+                        <h1>$114,678.2829</h1>
+                        <p>$443 (+0.05%) Today</p>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+
+}
+
+export default Newsfeed;
---
+++ src/Stats.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Stats(){
+
+    return(
+
+        <div className="stats">
+
+        </div>
+
+    )
+
+}
+
+export default Stats;
