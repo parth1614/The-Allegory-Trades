9e51eebccfe0a7e45d4b4a5b657f07fe77c0831f
Parth Arora <60436959+parth1614@users.noreply.github.com>
2021-05-10 09:47:51 +0530

    Update Newsfeed.js

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Newsfeed.css'
+import LineGraph from './LineGraph'
 
 function Newsfeed(){
 
@@ -11,6 +12,9 @@ function Newsfeed(){
                     <div className="newsfeed_portfolio">
                         <h1>$114,678.2829</h1>
                         <p>$443 (+0.05%) Today</p>
+                    </div>
+                    <div className="newsfeed_chart">
+                        <LineGraph />
                     </div>
                 </div>
 
